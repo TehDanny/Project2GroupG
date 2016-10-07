e9cbcc068fc6d01bb4c9b107de22bae78e8b74fc
Marcello Feroce <marcello.feroce@studio.unibo.it>
2016-10-07 13:01:45 +0200

    Done 26 suceeding tests bitches!

--- AbsenceRegistrationService/MsSqlPresenceDataMapper.cs
+++ AbsenceRegistrationService/MsSqlPresenceDataMapper.cs
@@ -8,12 +8,23 @@ namespace AbsenceRegistrationService
 {
     public class MsSqlPresenceDataMapper :MsSqlOperations,IPresenceDataMapper
     {
+        private string date;
+        private string time;
+        private string email;
+        private string ip;
+        private string mac;
+        private static Object thisLock = new Object();
+
         public LinkedList<UserPresence> ReadUserHistory(string key)
         {
-            LinkedList<int> indexes = base.GetTimeIndexesFromEmail(key);
+            LinkedList<int> indexes = new LinkedList<int>();
+            lock (MsSqlPresenceDataMapper.thisLock)
+            {
+                indexes = base.GetTimeIndexesFromEmail(key);
+            }
             LinkedList<UserPresence> userPresences = new LinkedList<UserPresence>();
             string indexesString = string.Join(",", indexes.ToArray());
-            sqlCommandString = "select dateaut,timeaut,mac,ip from Project2GroupGAutenticationTable where timeindex in+'" + indexesString + "'";
+            string sqlCommandString = "select dateaut,timeaut,mac,ip from Project2GroupGAutenticationTable where timeindex in+'" + indexesString + "'";
             base.Connect();
             cmd = new SqlCommand(sqlCommandString, base.GetSqlConnection());
             reader = cmd.ExecuteReader();
@@ -37,7 +48,7 @@ namespace AbsenceRegistrationService
         public LinkedList<UserPresence> ReadAllUsersHistory()
         {
             LinkedList<UserPresence> userPresences = new LinkedList<UserPresence>();
-            sqlCommandString = "select email,dateaut,timeaut,mac,ip from Project2GroupGAutenticationTable";
+            string sqlCommandString = "select email,dateaut,timeaut,mac,ip from Project2GroupGAutenticationTable";
             base.Connect();
             cmd = new SqlCommand(sqlCommandString, base.GetSqlConnection());
             reader = cmd.ExecuteReader();
@@ -61,15 +72,33 @@ namespace AbsenceRegistrationService
         }
         public void Create(UserPresence obj)
         {
-            sqlCommandString = "INSERT INTO  Project2GroupGAutenticationTable (dateaut,timeaut, mac,ip) VALUES('" + obj.GetDate().ToString("yyyy-MM-dd HH:mm:ss.fff") + "','" + obj.GetDate().TimeOfDay + "', " + "'" + obj.GetMac() + "','"+obj.GetIp()+"');";
-            this.DoVoidCommand(this.sqlCommandString);
-            int lastindex = base.LastIndexAutenticationTable();
-            sqlCommandString = "INSERT INTO  Project2GroupGAutenticationUserTable (timeindex,email) VALUES('" + lastindex + "','" + obj.GetEmail() + "');";
-            this.DoVoidCommand(this.sqlCommandString);
+            lock (thisLock)
+            {
+                this.SetParameters(obj);
+                string sqlCommandString = "INSERT INTO  Project2GroupGAutenticationTable (dateaut,timeaut, mac,ip) VALUES(" + this.date + "," + this.time + ", " + "" + this.mac + "," + this.ip + ");";
+                this.DoVoidCommand(sqlCommandString);
+                int lastindex = base.LastIndexAutenticationTable();
+                sqlCommandString = "INSERT INTO  Project2GroupGAutenticationUserTable (timeindex,email) VALUES('" + lastindex + "'," + this.email + ");";
+                this.DoVoidCommand(sqlCommandString);
+            }
+            
+            
         }
+
+        private void SetParameters(UserPresence obj)
+        {
+            this.date = (obj.GetDate() == null) ? "NULL" : ("'" + obj.GetDate().ToString("yyyy-MM-dd HH:mm:ss.fff") + "'");
+            this.time = (obj.GetDate().TimeOfDay == null) ? "NULL" : ("'" + obj.GetDate().TimeOfDay + "'");
+            this.email = (obj.GetEmail() == null) ? "NULL" : ("'" + obj.GetEmail() + "'");
+            this.ip = (obj.GetIp() == null) ? "NULL" : ("'" + obj.GetIp() + "'");
+            this.mac = (obj.GetMac() == null) ? "NULL" : ("'" + obj.GetMac() + "'");
+        }
+
         public UserPresence Read(string key)//last autentication
         {
-            int index=base.GetLastTimeIndexFromEmail(key);
+            string sqlCommandString;
+            this.SetParameters(new UserPresence(default(DateTime),key,null,null));
+            int index = base.GetLastTimeIndexFromEmail(key);
             if (index == 0) return null;
             base.Connect();
             sqlCommandString = "select dateaut,timeaut,mac,ip from Project2GroupGAutenticationTable where timeindex='" + index + "'";
@@ -93,16 +122,24 @@ namespace AbsenceRegistrationService
         }
         public void Update(UserPresence obj)//update last autentication time for that user
         {
-            int index = base.GetLastTimeIndexFromEmail(obj.GetEmail());
-            this.sqlCommandString = "UPDATE  Project2GroupGAutenticationTable SET dateaut='" + obj.GetDate().ToString("yyyy-MM-dd HH:mm:ss.fff") + "',timeaut='" + obj.GetDate().TimeOfDay + "', mac='" + obj.GetMac() + "',ip='" + obj.GetIp() + "' WHERE timeindex='"+index+"';";
-            this.DoVoidCommand(this.sqlCommandString);
+            lock (thisLock)
+            {
+                this.SetParameters(obj);
+                int index = base.GetLastTimeIndexFromEmail(obj.GetEmail());
+                string sqlCommandString = "UPDATE  Project2GroupGAutenticationTable SET dateaut=" + this.date + ",timeaut=" + this.time + ", mac=" + this.mac + ",ip=" + this.ip + " WHERE timeindex='" + index + "';";
+                this.DoVoidCommand(sqlCommandString);
+            }
+            
         }
 
         public void Delete(string key)
         {
-            int index = base.GetLastTimeIndexFromEmail(key);
-            this.sqlCommandString = "DELETE FROM  Project2GroupGAutenticationTable WHERE timeindex='" + index + "';";
-            this.DoVoidCommand(this.sqlCommandString);
+            lock (thisLock)
+            {
+                int index = base.GetLastTimeIndexFromEmail(key);
+                string sqlCommandString = "DELETE FROM  Project2GroupGAutenticationTable WHERE timeindex='" + index + "';";
+                this.DoVoidCommand(sqlCommandString);
+            }
         }
     }
 }
